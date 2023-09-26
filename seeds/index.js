b8b6e5f9c4256254dddb5156e2718d58bbb2fe48
Jonathan Maldonado Guemarez <cantor.dedekind@gmail.com>
2023-09-26 17:30:50 -0400

    2nd commit: Another comment added to the main seeding file, noticing that each time the seedAll function gets invoked, the previously stored data gets deleted.

--- seeds/index.js
+++ seeds/index.js
@@ -7,7 +7,8 @@ const seedProductTags = require('./product-tag-seeds');
 const sequelize = require('../config/connection');
 
 /*Asynchronous function that will seed all the tables in the db by sequentially invoking 
-the seeding functions exported by each of the required modules. At each step of the seeding, a succes message will be logged to the terminal*/
+the seeding functions exported by each of the required modules. At each step of the seeding, a succes message will be logged to the terminal.
+Every time the db is re-seeded, the already saved data will be deleted due to the { force: true } argument passed to the .sync method*/
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');

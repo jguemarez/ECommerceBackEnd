6a51e90fbafc5e38839a6a65ad90095336e314d8
Jonathan Maldonado Guemarez <cantor.dedekind@gmail.com>
2023-09-26 17:33:59 -0400

    3rd commit: The fields for the Sequelize models have been defined with the required constraints. The associations between the various models are made explicit by def. them in pairs within the index.js file.

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,22 @@
+//Import the necessary objects from Sequelize
 const { Model, DataTypes } = require('sequelize');
-
+//Require the sequelize connection to sync the model with the MySQL db.
 const sequelize = require('../config/connection.js');
-
+//Stipulate that 'Category' is achild class of 'Model'
 class Category extends Model {}
-
+//Initializing (eq. to sequelize.define) the model
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
@@ -16,5 +26,5 @@ Category.init(
     modelName: 'category',
   }
 );
-
+//Exporting the model so other modules can use it
 module.exports = Category;
--- models/index.js
+++ models/index.js
@@ -1,16 +1,41 @@
-// import models
+// Import the Sequelize models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Defining the one-to-many relation between the tables corresponding to Category (source) and Product (target)
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+})
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
-// Products belongToMany Tags (through ProductTag)
+//Defining the many-to-many relation between the tables corresponding to Product and Tag
 
-// Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Stipulate the junction model where the foreign-key will be stored
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'product_tags'
+});
+
+Tag.belongsToMany(Product, {
+  // Stipulate the junction model where the foreign-key will be stored
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved.
+  as: 'tag_products'
+});
+
 
 module.exports = {
   Product,

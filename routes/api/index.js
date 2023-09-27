14f79997fb302d47cf1f09a1c02e78322fc21642
Jonathan Maldonado Guemarez <cantor.dedekind@gmail.com>
2023-09-27 13:43:26 -0400

    8th commit: Added a new endpoint  with a get routes to retrieve all of the product_tags linking the products and the tags in many to many relations. Imported the corresponding router to the index.js file and initialized the middleware.

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,9 +4,11 @@ const router = require('express').Router();
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
+const prouduct_tagRoutes = require('./product_tag-routes');
 //Turning on the router for the aforementioned routes. 
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
+router.use('/product_tags', prouduct_tagRoutes);
 //Exporting the router
 module.exports = router;
